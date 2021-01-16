f60fefd76e433dee3ee5b447f65942873d1f2838
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-01-17 04:11:17 +0530

    Update Tests

--- test/client-test.js
+++ test/client-test.js
@@ -5,7 +5,7 @@ const clash = require('../index')
 
 describe('test creating game', () => {
     it('game returned must not be null', async () => {
-        let client = await clash.createClashClient(process.env.TEST_EMAIL, process.env.TEST_PASS)
+        let client = await clash.createClashClient(process.env.test_email, process.env.test_pass)
         let game = await client.createPrivateGame()
         assert(game != null);
         assert(game['publicHandle'] != null);
